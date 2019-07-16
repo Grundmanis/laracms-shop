0ec4c125d8ffacd8b7f3252d37a78062d85b8b85
Aruna Priyankara Wijewardana <aruna.wijewardana@gmail.com>
2019-07-16 17:04:04 +0300

    1-amazon-s3-Small fixes for the method names in UplodImage.

--- src/Models/ProductImage.php
+++ src/Models/ProductImage.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductImage extends Model
 {
-    protected $fillable = ['product_id', 'url'];
+    protected $fillable = ['product_id','original_image','old_original_image', 'url', 'thumbnail'];
 }
